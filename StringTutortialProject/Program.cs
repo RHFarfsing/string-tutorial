9cc9cf7debb365fc90be25e1731e16826d9c9d9d
Robert Farfsing <robertfarfsing@gmail.com>
2020-02-10 13:57:43 -0500

    string tutorial

---
+++ StringTutortialProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StringTutortialProject {
+    class Program {
+        static void Main(string[] args) {
+            var st1 = new StingsTutorial();
+            var fullname = st1.Fullname();
+            Console.WriteLine($"Fullname is {fullname}.");
+            Console.WriteLine($"Fullname2 is {st1.Fullname2()}.");
+            var denise = new StingsTutorial("Denise", "Barktick");
+            Console.WriteLine($"Denise {denise.Fullname()}");
+            Console.WriteLine($"Denise {denise.Fullname2()}");
+            }
+        }
+    }
---
+++ StringTutortialProject/StingsTutorial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringTutortialProject {
+    class StingsTutorial {
+        public string Firstname;
+        public string Lastname;
+        public StingsTutorial() {
+            Firstname = "John";
+            Lastname = "Farfsing";
+            }
+        public StingsTutorial(string firstname, string lastname) {
+            Firstname = firstname;
+            Lastname = lastname;
+            }
+        public string Fullname() {
+            //Don't need the vars in the methods because we have them as properties
+            //var Firstname = "Robbie";
+            //var Lastname = "Farfsing";
+            //var Fullname = Firstname + " " + Lastname;
+            var fullname = $"{Lastname}, {Firstname}";
+            return fullname;
+            }
+        public string Fullname2() {
+            //var Firstname = "Robbie";
+            //var Lastname = "Farfsing";
+            var fullname = $"{Firstname}, {Lastname}";
+            return fullname; 
+            }
+        }
+    }   
